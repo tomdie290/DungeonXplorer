a2bc41b75eae9006fdb595e8bbf0f9a5a65b8c58
Buzzy35 <imbuzzy35@gmail.com>
2025-11-24 15:48:44 +0100

    Ajout de l'archi MVC

--- views/404.php
+++ views/404.php
@@ -6,8 +6,21 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>404</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
-<h1>Error 404 Not Found</h1>
+<div class="custom-bg text-dark">
+    <div class="d-flex align-items-center justify-content-center min-vh-100 px-2">
+        <div class="text-center">
+            <h1 class="display-1 fw-bold">404</h1>
+            <p class="fs-2 fw-medium mt-4">Oops! Page not found</p>
+            <p class="mt-4 mb-5">The page you're looking for doesn't exist or has been moved.</p>
+            <a href="/" class="btn btn-light fw-semibold rounded-pill px-4 py-2 custom-btn">
+                Go Home
+            </a>
+        </div>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
