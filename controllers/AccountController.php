32c47e15ff4dd9c63d7960c114f15873c995aefa
Mathieu Degroux <degroux241@unicaen.fr>
2025-11-25 11:29:06 +0100

    fix account

--- controllers/AccountController.php
+++ controllers/AccountController.php
@@ -1,9 +1,12 @@
 <?php
 
-class AccountController
-{
+class AccountController {
     public function index() {
-        require_once 'view/account.php';
+        if (!isset($_SESSION['id']) || !isset($_SESSION['username'])) {
+            require_once 'view/home.php';
+        } else {
+            require_once 'view/account.php';
+        }
     }
 }
 ?>
