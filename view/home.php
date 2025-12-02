8d1ef40d1665e947a1cd7650ba7286c3b3eac974
Buzzy35 <imbuzzy35@gmail.com>
2025-12-02 14:10:54 +0100

    text

--- view/home.php
+++ view/home.php
@@ -15,6 +15,21 @@
         <a href="login" class="btn btn-primary w-50">Se connecter</a>
         <a href="register" class="btn btn-primary w-50">Créer un compte</a>
     </div>
+    <div>
+        <p class="texte-principal">
+            <br>
+            Bienvenue sur DungeonXplorer, l'univers de dark fantasy où se mêlent aventure, stratégie et immersion
+            totale dans les récits interactifs.
+            Ce projet est né de la volonté de l’association Les Aventuriers du Val Perdu de raviver l’expérience unique
+            des livres dont vous êtes le héros. Notre vision : offrir à la communauté un espace où chacun peut
+            incarner un personnage et plonger dans des quêtes épiques et personnalisées.
+            Dans sa première version, DungeonXplorer permettra aux joueurs de créer un personnage parmi trois
+            classes emblématiques — guerrier, voleur, magicien — et d’évoluer dans un scénario captivant, tout en
+            assurant à chacun la possibilité de conserver sa progression.
+            Nous sommes enthousiastes de partager avec vous cette application et espérons qu'elle saura vous
+            plonger au cœur des mystères du Val Perdu !
+        </p>
+    </div>
 </div>
 
 </body>
