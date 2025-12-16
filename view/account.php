c8b2408a9af3cd35d1af851c5be61bfc3ba3d337
Die tom <tom.die@etu.unicaen.fr>
2025-12-16 17:55:42 +0100

    modification page accueil

--- view/account.php
+++ view/account.php
@@ -39,7 +39,26 @@ $heroes = $stmt->fetchAll(PDO::FETCH_ASSOC);
     </head>
     <?php require_once 'navbar.php'; ?>
     <body>
+        <div class="container">
+        <div>
+            <h1 class="login-title mt-5 mb-4">Bienvenue <?= htmlspecialchars($_SESSION['username']) ?> !</h1>
+        <p class="texte-principal">
+            <br>
+            Bienvenue sur DungeonXplorer, l'univers de dark fantasy où se mêlent aventure, stratégie et immersion
+            totale dans les récits interactifs.
+            Ce projet est né de la volonté de l’association Les Aventuriers du Val Perdu de raviver l’expérience unique
+            des livres dont vous êtes le héros. Notre vision : offrir à la communauté un espace où chacun peut
+            incarner un personnage et plonger dans des quêtes épiques et personnalisées.
+            Dans sa première version, DungeonXplorer permettra aux joueurs de créer un personnage parmi trois
+            classes emblématiques — guerrier, voleur, magicien — et d’évoluer dans un scénario captivant, tout en
+            assurant à chacun la possibilité de conserver sa progression.
+            Nous sommes enthousiastes de partager avec vous cette application et espérons qu'elle saura vous
+            plonger au cœur des mystères du Val Perdu !
+        </p>
+    </div>
+    </div>
         <h2 class="login-title mt-5 mb-4">Liste de mes héros</h2>
+        
 
 <div class="container">
     <?php if (empty($heroes)): ?>
